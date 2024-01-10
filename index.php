1054ce6c134af648182f2fcfe7797a4e76705af8
Prashon Gautam <prashon109939@gmail.com>
2024-01-10 09:57:27 +0545

    3rd commit

--- index.php
+++ index.php
@@ -54,7 +54,19 @@ require_once "header.php";
             </div>
         </div>
     </div>
-</div>
-<?php
-require_once "footer.php";
-?>
+    <div class="col-md-3">
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+
+
+
+        <?php
+        require_once "footer.php";
+        ?>
