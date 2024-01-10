5ec0ab4a37de14e5dc6f6361e83009a7fd2bb75a
Prashon Gautam <prashon109939@gmail.com>
2024-01-10 09:49:49 +0545

    2nd commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once "header.php";
 ?>
 
 
-<h1>Books List</h1>
+<!-- <h1>Books</h1> -->
 
 <div class="row">
     <div class="col-md-12">
@@ -36,7 +36,25 @@ require_once "header.php";
     </div>
 </div>
 
+<div class="row">
+    <div class="col-md-12">
+        <h1>Books List</h1>
+    </div>
+</div>
 
+<div class="row">
+    <div class="col-md-3">
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+</div>
 <?php
 require_once "footer.php";
 ?>
